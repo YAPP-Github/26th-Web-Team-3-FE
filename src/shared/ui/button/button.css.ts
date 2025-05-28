e849b7b3556c3818e220354bc42979a0fc193b7d
Hanseo Kim <108131226+seueooo@users.noreply.github.com>
2025-05-28 11:33:40 +0900

    Init: 프로젝트 생성 및 라이브러리 설치 (#5)
    * init: 프로젝트 생성
    
    * init: vanilla-extract 및 플러그인 설치
    
    * init: 불필요한 파일 삭제 및 버튼 컴포넌트 예시 추가
    
    * docs: 불필요한 파일 삭제
    
    * init: clsx 추가
    
    * init: 초기 스타일 파일 생성
    
    * init: ky, tanstack-query 설치
    
    * chore: gitkeep 파일 추가

---
+++ src/shared/ui/button/button.css.ts
@@ -0,0 +1,8 @@
+import { style } from "@vanilla-extract/css";
+
+// TODO: 추후 디자인에 맞게 변경, 스타일 예시 코드
+export const buttonStyle = style({
+  borderRadius: "10px",
+  backgroundColor: "#8c92ff",
+  fontSize: "24px",
+});
---
+++ src/shared/ui/button/button.tsx
@@ -0,0 +1,10 @@
+import * as styles from "./button.css";
+
+interface ButtonProps {}
+
+// TODO: 추후 수정
+const Button = ({}: ButtonProps) => {
+  return <button className={styles.buttonStyle}>Button</button>;
+};
+
+export default Button;
