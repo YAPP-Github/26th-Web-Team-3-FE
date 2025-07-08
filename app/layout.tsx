7c8071a586c985eaa75405acb52cc25a46f1bcbf
beom <bdh3659@naver.com>
2025-07-08 10:55:39 +0900

    feat: layout에 pretendard 기본으로 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,9 @@
 import Providers from "@/app/providers";
+import { pretendard } from "@/shared/styles/base/font.css";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import type { ReactNode } from "react";
 
 // TODO: 추후 수정
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,8 +16,8 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <html lang="ko">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="ko" className={pretendard.className}>
+      <body>
         <Providers>{children}</Providers>
       </body>
     </html>
