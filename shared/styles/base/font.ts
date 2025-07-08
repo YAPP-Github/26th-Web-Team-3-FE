a36398e207b7dec8d2748855c2f547bb307391f6
beom <bdh3659@naver.com>
2025-07-08 16:52:32 +0900

    chore: font path 수정

--- shared/styles/base/font.ts
+++ shared/styles/base/font.ts
@@ -8,17 +8,17 @@ export const pretendard = localFont({
       style: "normal",
     },
     {
-      path: "../../../node_modules/pretendard/dist/web/static/woff2/Pretendard-Regular.woff2",
+      path: "../../../node_modules/pretendard/dist/web/static/woff2/Pretendard-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../../../node_modules/pretendard/dist/web/static/woff2/Pretendard-Regular.woff2",
+      path: "../../../node_modules/pretendard/dist/web/static/woff2/Pretendard-SemiBold.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../../../node_modules/pretendard/dist/web/static/woff2/Pretendard-Regular.woff2",
+      path: "../../../node_modules/pretendard/dist/web/static/woff2/Pretendard-Bold.woff2",
       weight: "700",
       style: "normal",
     },
