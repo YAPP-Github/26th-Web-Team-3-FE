1381de4579d85ac6510f1e1322dca1a672913b22
hanseo <yds06206@gmail.com>
2025-07-12 17:07:17 +0900

    feat: svgr 설정

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,27 @@
-import type { NextConfig } from "next";
 import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
+import type { NextConfig } from "next";
+import withSvgr from "next-plugin-svgr";
 
-const withVanillaExtract = createVanillaExtractPlugin();
+// Vanilla Extract 플러그인
+const withVanillaExtract = (config: NextConfig): NextConfig =>
+  createVanillaExtractPlugin()(config);
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// Svgr 플러그인
+const withCustomSvgr = (config: NextConfig): NextConfig =>
+  withSvgr({
+    ...config,
+    svgrOptions: {
+      dimensions: false,
+      icon: true,
+    },
+  });
+
+const plugins = [withVanillaExtract, withCustomSvgr];
+
+const baseConfig: NextConfig = {
+  reactStrictMode: true,
 };
 
-export default withVanillaExtract(nextConfig);
+const finalConfig = plugins.reduce((acc, plugin) => plugin(acc), baseConfig);
+
+export default finalConfig;
