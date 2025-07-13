538151b66a372d85ee43eb8649505ae3d577e057
hanseo <yds06206@gmail.com>
2025-07-14 02:39:58 +0900

    style: 햄버거바 로테이션 애니메이션 추가 및 반응형 메뉴 구현

--- shared/ui/nav-home/nav-home.css.ts
+++ shared/ui/nav-home/nav-home.css.ts
@@ -53,6 +53,9 @@ export const buttonStyle = style({
 });
 
 export const searchButtonStyle = style({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
   width: "4.4rem",
   height: "4.4rem",
 });
@@ -60,7 +63,89 @@ export const searchButtonStyle = style({
 export const menuButtonStyle = style({
   width: "4.4rem",
   height: "4.4rem",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  gap: "0.5rem",
   ...screenTheme.md({
     display: "none",
   }),
+});
+
+export const hamburgerIcon = style({
+  position: "relative",
+  width: "2rem",
+  height: "2rem",
+});
+
+export const hamburgerLine = style({
+  position: "absolute",
+  left: "0",
+  width: "100%",
+  height: "2px",
+  borderRadius: "10px",
+  backgroundColor: colorTheme.white[100],
+  transition: "all 0.2s ease-in-out",
+});
+
+export const hamburgerLineTop = style([
+  hamburgerLine,
+  {
+    top: "2px",
+  },
+]);
+
+export const hamburgerLineMiddle = style([
+  hamburgerLine,
+  {
+    top: "50%",
+    transform: "translateY(-50%)",
+  },
+]);
+
+export const hamburgerLineBottom = style([
+  hamburgerLine,
+  {
+    bottom: "2px",
+  },
+]);
+
+export const xIconTop = style({
+  transform: "rotate(45deg) translate(5px, 5px)",
+});
+
+export const xIconMiddle = style({
+  opacity: "0",
+});
+
+export const xIconBottom = style({
+  transform: "rotate(-45deg) translate(5px, -5px)",
+});
+
+export const menuContainer = style({
+  position: "fixed",
+  top: "6.4rem",
+  left: "0",
+  width: "100%",
+  backgroundColor: colorTheme.black["90_bg"],
+  zIndex: zIndex.header.content,
+  overflow: "hidden",
+  transition: "max-height 0.2s ease-out, opacity 0.3s ease",
+  maxHeight: 0,
+  opacity: 0,
+});
+
+export const menuContainerOpen = style({
+  maxHeight: "17.4rem",
+  opacity: 1,
+});
+
+export const menuItem = style({
+  padding: "1.6rem 2rem",
+  ...textTheme.caption16,
+  color: colorTheme.white[100],
+  cursor: "pointer",
+  ":hover": {
+    backgroundColor: colorTheme.white[5],
+  },
 });
--- shared/ui/nav-home/nav-home.tsx
+++ shared/ui/nav-home/nav-home.tsx
@@ -1,32 +1,79 @@
-import MenuIcon from "@/shared/assets/svgs/menu.svg";
+"use client";
+
 import SearchIcon from "@/shared/assets/svgs/search.svg";
+import { cn } from "@/shared/utils/cn";
 
 import * as styles from "./nav-home.css";
 
+import { useState } from "react";
+
 const NavHome = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleMenuClick = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
   return (
-    <nav className={styles.navHomeStyle}>
-      <div>logo</div>
-      <div className={styles.navWrapper}>
-        <ul className={styles.navList}>
-          <li>
-            <button className={styles.buttonStyle}>탐색</button>
-          </li>
-          <li>
-            <button className={styles.buttonStyle}>내 캡슐</button>
-          </li>
-          <li>
-            <button className={styles.buttonStyle}>설정</button>
-          </li>
-        </ul>
-        <button className={styles.searchButtonStyle}>
-          <SearchIcon width={"2rem"} height={"2rem"} />
-        </button>
-        <button className={styles.menuButtonStyle}>
-          <MenuIcon width={"2rem"} height={"2rem"} />
-        </button>
-      </div>
-    </nav>
+    <>
+      <nav className={styles.navHomeStyle}>
+        <div>logo</div>
+        <div className={styles.navWrapper}>
+          <ul className={styles.navList}>
+            <li>
+              <button className={styles.buttonStyle}>탐색</button>
+            </li>
+            <li>
+              <button className={styles.buttonStyle}>내 캡슐</button>
+            </li>
+            <li>
+              <button className={styles.buttonStyle}>설정</button>
+            </li>
+          </ul>
+          <button className={styles.searchButtonStyle}>
+            <SearchIcon width={"2rem"} height={"2rem"} />
+          </button>
+          {/* 메뉴버튼 클릭시 햄버거바 -> x버튼으로 rotate */}
+          <button className={styles.menuButtonStyle} onClick={handleMenuClick}>
+            <div className={styles.hamburgerIcon}>
+              <div
+                className={cn(
+                  styles.hamburgerLineTop,
+                  isMenuOpen ? styles.xIconTop : undefined,
+                )}
+              ></div>
+              <div
+                className={cn(
+                  styles.hamburgerLineMiddle,
+                  isMenuOpen ? styles.xIconMiddle : undefined,
+                )}
+              ></div>
+              <div
+                className={cn(
+                  styles.hamburgerLineBottom,
+                  isMenuOpen ? styles.xIconBottom : undefined,
+                )}
+              ></div>
+            </div>
+          </button>
+        </div>
+      </nav>
+      <ul
+        className={cn(
+          styles.menuContainer,
+          isMenuOpen ? styles.menuContainerOpen : "",
+        )}
+      >
+        <li className={styles.menuItem}>
+          <span>탐색</span>
+        </li>
+        <li className={styles.menuItem}>
+          <span>내 캡슐</span>
+        </li>
+        <li className={styles.menuItem}>
+          <span>설정</span>
+        </li>
+      </ul>
+    </>
   );
 };
 
