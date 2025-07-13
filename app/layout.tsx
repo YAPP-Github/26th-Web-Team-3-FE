f5e6c81aed415140761249e76bcd7e41afe4b4e8
hanseo <yds06206@gmail.com>
2025-07-13 18:26:31 +0900

    chore: 파일명 컨벤션에 맞게 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import "@/shared/styles/base/global.css";
 import "@/shared/styles/base/reset.css";
 import { rootStyle } from "@/shared/styles/base/global.css";
 
-import SprinkleContainer from "@/shared/ui/sprinkle-container/sprinkle-container";
+import SprinkleContainer from "@/shared/ui/sprinkle-container";
 
 // TODO: 추후 수정
 export const metadata: Metadata = {
