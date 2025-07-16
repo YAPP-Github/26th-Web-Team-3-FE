66dcd16685b716cedbffacbf27e6f77175f6bc78
beom <bdh3659@naver.com>
2025-07-17 08:16:30 +0900

    chore: next-plugin-svgr 삭제

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,11 @@
 import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
 import type { NextConfig } from "next";
-import withSvgr from "next-plugin-svgr";
 
 // Vanilla Extract 플러그인
 const withVanillaExtract = (config: NextConfig): NextConfig =>
   createVanillaExtractPlugin()(config);
 
-// Svgr 플러그인
-const withCustomSvgr = (config: NextConfig): NextConfig =>
-  withSvgr({
-    ...config,
-    svgrOptions: {
-      dimensions: false,
-      icon: true,
-    },
-  });
-
-const plugins = [withVanillaExtract, withCustomSvgr];
+const plugins = [withVanillaExtract];
 
 const baseConfig: NextConfig = {
   reactStrictMode: true,
