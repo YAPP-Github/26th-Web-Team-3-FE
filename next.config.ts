75d970f6c9ac17bbb082435d7be7bb2859f470a3
beom <bdh3659@naver.com>
2025-07-19 08:47:36 +0900

    chore: svgr/webpack 설치

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,22 @@
 import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
 import type { NextConfig } from "next";
 
+const withSvgr = (config: NextConfig): NextConfig => ({
+  ...config,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+});
+
 // Vanilla Extract 플러그인
 const withVanillaExtract = (config: NextConfig): NextConfig =>
   createVanillaExtractPlugin()(config);
 
-const plugins = [withVanillaExtract];
+const plugins = [withSvgr, withVanillaExtract];
 
 const baseConfig: NextConfig = {
   reactStrictMode: true,
