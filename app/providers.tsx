e90f7d944a0cd848c86879dcf86d04179776db2e
hanseo <yds06206@gmail.com>
2025-07-20 22:04:13 +0900

    feat: overlay-kit 패키지 설치 및 provider 생성

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,4 +1,7 @@
+"use client";
 import QueryProvider from "@/shared/providers/query-provider";
+import { OverlayProvider } from "overlay-kit";
+import { useEffect, useState } from "react";
 import type { ReactNode } from "react";
 
 interface ProvidersProps {
@@ -6,7 +9,17 @@ interface ProvidersProps {
 }
 
 const Providers = ({ children }: ProvidersProps) => {
-  return <QueryProvider>{children}</QueryProvider>;
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => setIsClient(true), []);
+
+  if (!isClient) return null;
+
+  return (
+    <QueryProvider>
+      <OverlayProvider>{children}</OverlayProvider>
+    </QueryProvider>
+  );
 };
 
 export default Providers;
