2f446e9a7296dc605a1349c6258b9c151c7e5bd2
hanseo <yds06206@gmail.com>
2025-07-22 02:45:56 +0900

    refactor: 필요없는 docs제거

--- shared/hooks/useInView.ts
+++ shared/hooks/useInView.ts
@@ -3,10 +3,9 @@ import { useEffect, useRef, useState } from "react";
 /**
  * useInView
  *
- * 특정 DOM 요소가 뷰포트(Viewport)에 보이는지 여부를 감지하는 커스텀 훅입니다.
- * IntersectionObserver API를 사용합니다.
+ * 특정 DOM 요소가 뷰포트에 보이는지 여부를 감지하는 커스텀 훅입니다.
  *
- * @param {number} [threshold=0.2] - 요소가 뷰포트에 얼마나 보여야 isIntersecting이 true가 되는지 결정하는 값 (0~1)
+ * @param {number} - 요소가 뷰포트에 얼마나 보여야 isIntersecting이 true가 되는지 결정하는 값 (0~1)
  * @returns {{
  *   ref: React.RefObject<HTMLElement>;
  *   isIntersecting: boolean;
@@ -15,7 +14,6 @@ import { useEffect, useRef, useState } from "react";
  * @example
  * const { ref, isIntersecting } = useInView(0.5);
  *
- * // 이때 div는 뷰포트에 보이는지 여부를 감지할 요소입니다. 뷰포트 화면 전체 넓이로 설정해야 합니다.
  * return <div ref={ref}>{isIntersecting ? "보임" : "안 보임"}</div>;
  */
 export const useInView = (threshold = 0.2) => {
@@ -23,18 +21,19 @@ export const useInView = (threshold = 0.2) => {
   const [isIntersecting, setIsIntersecting] = useState(false);
 
   useEffect(() => {
+    const currentElement = ref.current;
     const observer = new IntersectionObserver(
       ([entry]) => {
         setIsIntersecting(entry.isIntersecting);
       },
       { threshold },
     );
-    if (ref.current) {
-      observer.observe(ref.current);
+    if (currentElement) {
+      observer.observe(currentElement);
     }
     return () => {
-      if (ref.current) {
-        observer.unobserve(ref.current);
+      if (currentElement) {
+        observer.unobserve(currentElement);
       }
     };
   }, [threshold]);
