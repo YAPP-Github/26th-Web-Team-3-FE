7bb79596a42f46e5ebd1f341fa05cdd5167c0544
hanseo <yds06206@gmail.com>
2025-07-21 21:37:19 +0900

    chore: 타입 파일 추가

--- shared/types/assets.d.ts
+++ shared/types/assets.d.ts
@@ -1,3 +1,13 @@
+declare module "*.png" {
+  const value: {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL: string;
+  };
+  export = value;
+}
+
 declare module "*.svg" {
   import type { SVGProps } from "react";
   const ReactComponent: FunctionComponent<
