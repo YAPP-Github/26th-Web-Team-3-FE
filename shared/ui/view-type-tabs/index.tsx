4679cd925f3b4045591d1b3398ab2e0b80811758
Hanseo Kim <108131226+seueooo@users.noreply.github.com>
2025-07-23 10:45:56 +0900

    Feat: view-type-tabs 구현 및 스토리 작성 (#86)
    * feat: view-type-tabs 구현 및 스토리 작성
    
    * refactor: 코드래빗 리뷰 반영
    
    * chore: 폴더 구조 수정

---
+++ shared/ui/view-type-tabs/index.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import GridIcon from "@/shared/assets/icon/grid.svg";
+import LayerIcon from "@/shared/assets/icon/layers.svg";
+import { cn } from "@/shared/utils/cn";
+
+import { ViewType } from "@/shared/types/types";
+import * as styles from "./view-type-tabs.css";
+
+interface ViewTypeTabsProps {
+  viewType: ViewType;
+  handleClick: (viewType: ViewType) => void;
+}
+
+const ViewTypeTabs = ({ viewType, handleClick }: ViewTypeTabsProps) => {
+  return (
+    <div className={styles.viewTypeTabs}>
+      <button
+        onClick={() => handleClick(ViewType.LAYERS)}
+        className={cn(
+          styles.tabButton({ isSelected: viewType === ViewType.LAYERS }),
+          styles.iconColor({ isSelected: viewType === ViewType.LAYERS }),
+        )}
+        type="button"
+        aria-label="Layers"
+      >
+        <LayerIcon />
+        {viewType === ViewType.LAYERS && ViewType.LAYERS}
+      </button>
+      <button
+        onClick={() => handleClick(ViewType.GRID)}
+        className={cn(
+          styles.tabButton({ isSelected: viewType === ViewType.GRID }),
+          styles.iconColor({ isSelected: viewType === ViewType.GRID }),
+        )}
+        type="button"
+        aria-label="Grid"
+      >
+        <GridIcon />
+        {viewType === ViewType.GRID && ViewType.GRID}
+      </button>
+    </div>
+  );
+};
+
+export default ViewTypeTabs;
