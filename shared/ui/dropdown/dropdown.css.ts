687e7e03132114f8f993884a54beee7017fcaa47
Hanseo Kim <108131226+seueooo@users.noreply.github.com>
2025-07-25 20:55:01 +0900

    Feat: dropdown, navbar-detail 컴포넌트 구현 (#89)
    * feat: 드롭다운 컴포넌트 구현 및 스토리 작성
    
    * feat: navbar-detail 구현
    
    * style: 네브바 누락된 스타일 추가
    
    * chore: 코드 수정
    
    * refactor: 코드리뷰 반영

---
+++ shared/ui/dropdown/dropdown.css.ts
@@ -0,0 +1,39 @@
+import { themeVars } from "@/shared/styles/base/theme.css";
+import { style } from "@vanilla-extract/css";
+
+export const dropdownWrapper = style({
+  position: "relative",
+  borderRadius: "15px",
+});
+
+export const triggerBtnStyle = style({
+  padding: "1.1rem",
+  color: themeVars.color.white[60],
+});
+
+export const dropdownContent = style({
+  position: "absolute",
+  right: 0,
+  top: "100%",
+  display: "flex",
+  width: "19.2rem",
+  flexDirection: "column",
+  backgroundColor: themeVars.color.black[70],
+  padding: "0.6rem",
+  borderRadius: "15px",
+});
+
+export const dropdownItem = style({
+  display: "flex",
+  width: "100%",
+  padding: "1.6rem 2rem",
+  whiteSpace: "nowrap",
+  alignItems: "center",
+  ...themeVars.text.F16,
+  color: themeVars.color.white[100],
+  selectors: {
+    "&:hover": {
+      backgroundColor: themeVars.color.white[5],
+    },
+  },
+});
