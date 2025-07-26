e01f1d5f157666d469bc5126d0cc9bc21950fc04
hanseo <yds06206@gmail.com>
2025-07-26 18:18:57 +0900

    style: caption 스타일 추가

--- app/(max800px)/capsule-detail/_components/caption-section/caption-section.css.ts
+++ app/(max800px)/capsule-detail/_components/caption-section/caption-section.css.ts
@@ -11,8 +11,9 @@ export const container = style({
     margin: "1.6rem",
   }),
   width: "100%",
-  background: themeVars.color.gradient.white_op,
+  backgroundColor: themeVars.color.gradient.white_op,
   borderRadius: "16px",
   ...themeVars.text.B1,
   color: themeVars.color.white[85],
+  padding: "1.6rem 3rem",
 });
