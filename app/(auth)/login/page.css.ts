cb11ffdb143f232726bc8da6e7ad26797b12311a
hanseo <yds06206@gmail.com>
2025-07-29 05:27:53 +0900

    feat: 로그인 페이지 ui 구현

---
+++ app/(auth)/login/page.css.ts
@@ -0,0 +1,63 @@
+import { themeVars } from "@/shared/styles/base/theme.css";
+import { screen } from "@/shared/styles/tokens/screen";
+import { style } from "@vanilla-extract/css";
+
+export const header = style({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "100%",
+  height: "8rem",
+});
+
+export const contentsContainer = style({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  width: "100%",
+  marginTop: "4.8rem",
+  gap: "1rem",
+});
+
+export const title = style({
+  ...themeVars.text.H1,
+  textAlign: "center",
+});
+
+export const caption = style({
+  ...themeVars.text.B1,
+  textAlign: "center",
+  color: themeVars.color.white[60],
+});
+
+export const lettieImage = style({
+  width: "28rem",
+  height: "28rem",
+  ...screen.md({
+    width: "34rem",
+    height: "34rem",
+  }),
+});
+
+export const buttonContainer = style({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  maxWidth: "40rem",
+  width: "100%",
+  gap: "1.6rem",
+  marginBottom: "10.8rem",
+});
+
+export const button = style({
+  ...themeVars.text.F16,
+  color: themeVars.color.white[85],
+  display: "flex",
+  gap: "1.3rem",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "100%",
+  height: "5.2rem",
+  borderRadius: "12px",
+  backgroundColor: themeVars.color.black[80],
+});
