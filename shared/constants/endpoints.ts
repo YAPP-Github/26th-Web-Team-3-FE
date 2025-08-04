bd96031618c945cfcf37d9f847093293cd72acc3
beom <bdh3659@naver.com>
2025-08-04 22:49:34 +0900

    feat: CAPSULE_DETAIL 및 LETTER 관련 엔드포인트 함수형으로 변경

--- shared/constants/endpoints.ts
+++ shared/constants/endpoints.ts
@@ -1,5 +1,9 @@
 export const ENDPOINTS = {
   // 타임캡슐
   CREATE_CAPSULE: "api/v1/capsule",
-  CAPSULE_DETAIL: "api/v1/capsules/:id",
+  CAPSULE_DETAIL: (capsuleId: string) => `api/v1/capsules/${capsuleId}`,
+  // 편지
+  LETTER_LIST: (capsuleId: string, page = 0, size = 20) =>
+    `api/v1/letters?capsuleId=${capsuleId}&page=${page}&size=${size}`,
+  LETTER_DETAIL: (letterId: string) => `api/v1/letters/${letterId}`,
 } as const;
