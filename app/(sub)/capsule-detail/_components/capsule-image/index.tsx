60635295253fb9903057ed07b0802775000b4c59
hanseo <yds06206@gmail.com>
2025-08-07 02:18:14 +0900

    feat: 이미지 추가 및 이미지 도메인 config에 추가

--- app/(sub)/capsule-detail/_components/capsule-image/index.tsx
+++ app/(sub)/capsule-detail/_components/capsule-image/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import * as styles from "./capsule-image.css";
 
 interface CapsuleImageProps {
@@ -7,7 +8,7 @@ interface CapsuleImageProps {
 const CapsuleImage = ({ imageUrl }: CapsuleImageProps) => {
   return (
     <div className={styles.container}>
-      {/* <Image src={imageUrl} width={260} height={253} alt="캡슐 이미지" /> */}
+      <Image src={imageUrl} width={260} height={253} alt="apng" unoptimized />
     </div>
   );
 };
