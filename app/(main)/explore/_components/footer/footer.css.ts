bffae33a38a3371f60a856591781f77f8aaf0284
hanseo <yds06206@gmail.com>
2025-08-14 21:01:13 +0900

    style: 탐색페이지 캡슐 만들기 버튼 반응형 추가

--- app/(main)/explore/_components/footer/footer.css.ts
+++ app/(main)/explore/_components/footer/footer.css.ts
@@ -1,4 +1,5 @@
 import { themeVars } from "@/shared/styles/base/theme.css";
+import { screen } from "@/shared/styles/tokens/screen";
 import { style } from "@vanilla-extract/css";
 
 export const container = style({
@@ -24,4 +25,40 @@ export const footerTitle = style({
 export const footerDescription = style({
   ...themeVars.text.B2,
   color: themeVars.color.white[40],
+});
+
+export const mobileButton = style({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  color: themeVars.color.white[60],
+  width: "4.4rem",
+  height: "4.4rem",
+  borderRadius: "50%",
+  cursor: "pointer",
+  backgroundColor: themeVars.color.white[5],
+  selectors: {
+    "&:hover": {
+      backgroundColor: themeVars.color.white[15],
+    },
+  },
+  ...screen.md({
+    display: "none",
+    cursor: "default",
+  }),
+});
+
+export const desktopButtonContainer = style({
+  display: "none",
+  cursor: "default",
+  backgroundColor: themeVars.color.white[5],
+  selectors: {
+    "&:hover": {
+      backgroundColor: themeVars.color.white[15],
+    },
+  },
+  ...screen.md({
+    display: "flex",
+    cursor: "pointer",
+  }),
 });
--- app/(main)/explore/_components/footer/index.tsx
+++ app/(main)/explore/_components/footer/index.tsx
@@ -1,5 +1,7 @@
 import TabButton from "@/app/(main)/_components/tab-button";
+import PlusIcon from "@/shared/assets/icon/plus.svg";
 import { PATH } from "@/shared/constants/path";
+import Link from "next/link";
 import * as styles from "./footer.css";
 
 const Footer = () => {
@@ -12,7 +14,12 @@ const Footer = () => {
             타임캡슐을 만들고 사람들을 초대해보세요.
           </p>
         </div>
-        <TabButton text="타임캡슐 만들기" href={PATH.CREATE_CAPSULE} />
+        <div className={styles.desktopButtonContainer}>
+          <TabButton text="타임캡슐 만들기" href={PATH.CREATE_CAPSULE} />
+        </div>
+        <Link href={PATH.CREATE_CAPSULE} className={styles.mobileButton}>
+          <PlusIcon width={20} height={20} />
+        </Link>
       </footer>
     </div>
   );
