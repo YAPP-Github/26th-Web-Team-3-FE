70f76285a38a8de84c4566b00dfe51b8e5003302
hanseo <yds06206@gmail.com>
2025-08-16 22:16:36 +0900

    style: 테두리 인터렉션 구현

--- app/(main)/_components/home-button-section/home-button-section.css.ts
+++ app/(main)/_components/home-button-section/home-button-section.css.ts
@@ -1,6 +1,18 @@
 import { themeVars } from "@/shared/styles/base/theme.css";
 import { screen } from "@/shared/styles/tokens/screen";
-import { style } from "@vanilla-extract/css";
+import { keyframes, style } from "@vanilla-extract/css";
+
+const rotateMirrorAnimation = keyframes({
+  "0%": {
+    transform: "rotate(0deg)",
+  },
+  "50%": {
+    transform: "rotate(360deg)",
+  },
+  "100%": {
+    transform: "rotate(0deg)",
+  },
+});
 
 export const container = style({
   display: "flex",
@@ -23,25 +35,35 @@ export const button = style({
   backgroundColor: themeVars.color.black[70],
 });
 
-export const gradientButton = style({
+export const exploreButton = style({
   background: themeVars.color.gradient.purple,
   ...themeVars.text.B1,
   cursor: "pointer",
   padding: "0.9rem 2.5rem",
   borderRadius: "13px",
-  height: "5rem",
   position: "relative",
+});
 
-  "::after": {
-    content: '""',
-    position: "absolute",
-    top: "-2px",
-    left: "-2px",
-    width: "calc(100% + 4px)",
-    height: "calc(100% + 4px)",
-    zIndex: -1,
-    borderRadius: "13px",
-    background:
-      "conic-gradient(from 0deg at 50% 50%, #ffffff 3deg, rgba(255, 255, 255, 0) 50deg, rgba(234, 234, 234, 0) 126deg, rgba(253, 253, 253, 0.89285) 178deg, rgba(210, 210, 210, 0) 234deg, rgba(184, 184, 184, 0) 309deg, rgb(255, 255, 255) 360deg)",
-  },
+export const lineInteract = style({
+  position: "absolute",
+  top: "-160%",
+  left: "-10%",
+  zIndex: -1,
+  width: "219px",
+  height: "219px",
+  borderRadius: "99px",
+  opacity: 1,
+  background:
+    "conic-gradient(from 0deg at 50% 50%, rgb(255, 255, 255) 3.6deg, rgba(255, 255, 255, 0) 50.4deg, rgba(234, 234, 234, 0) 126deg, rgba(253, 253, 253, 0.89) 178.378deg, rgba(210, 210, 210, 0) 234deg, rgba(184, 184, 184, 0) 309.6deg, rgb(255, 255, 255) 360deg)",
+  animation: `${rotateMirrorAnimation} 5s cubic-bezier(0.44, 0, 0.56, 1) infinite`,
+});
+
+export const lineInteractContainer = style({
+  position: "absolute",
+  overflow: "hidden",
+  top: "-1px",
+  left: "-1px",
+  width: "calc(100% + 2px)",
+  height: "calc(100% + 2px)",
+  borderRadius: "13px",
 });
--- app/(main)/_components/home-button-section/index.tsx
+++ app/(main)/_components/home-button-section/index.tsx
@@ -7,13 +7,19 @@ import * as styles from "./home-button-section.css";
 const HomeButtonSection = () => {
   return (
     <div className={styles.container}>
-      <Link href={PATH.CREATE_CAPSULE}>
-        <button type="button" className={styles.gradientButton}>
+      <Link href={PATH.EXPLORE}>
+        <button className={styles.exploreButton}>
           타임캡슐 보러가기
+          <div className={styles.lineInteractContainer}>
+            <div className={styles.lineInteract}></div>
+          </div>
         </button>
       </Link>
-      <Link href={PATH.EXPLORE}>
-        <button className={styles.button}>타임캡슐 보러가기</button>
+
+      <Link href={PATH.CREATE_CAPSULE}>
+        <button type="button" className={styles.button}>
+          타임캡슐 만들기
+        </button>
       </Link>
     </div>
   );
