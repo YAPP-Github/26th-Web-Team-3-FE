e212c4c1a6649687d8c6f0b9d258dfec4d8b95f4
hanseo <yds06206@gmail.com>
2025-08-20 20:53:23 +0900

    feat: not-found 페이지 추가

---
+++ app/not-found.css.ts
@@ -0,0 +1,37 @@
+import { style } from "@vanilla-extract/css";
+import { themeVars } from "@/shared/styles/base/theme.css";
+
+export const container = style({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "100%",
+  height: "100%",
+});
+
+export const title = style({
+  ...themeVars.text.F17,
+  color: themeVars.color.white[100],
+  marginTop: "2rem",
+  marginBottom: "0.6rem",
+});
+
+export const description = style({
+  ...themeVars.text.F16,
+  color: themeVars.color.white[40],
+});
+
+export const button = style({
+  marginTop: "1.6rem",
+  ...themeVars.text.F14,
+  backgroundColor: themeVars.color.white[100],
+  color: themeVars.color.black[100],
+  borderRadius: "8px",
+  padding: "0.8rem 1.2rem",
+  selectors: {
+    "&:hover": {
+      backgroundColor: themeVars.color.white[70],
+    },
+  },
+});
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,18 @@
+import NoneImage from "@/shared/assets/2D-illust/none.svg";
+import * as styles from "./not-found.css";
+import Link from "next/link";
+import { PATH } from "@/shared/constants/path";
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className={styles.container}>
+      <NoneImage width={120} height={65} />
+      <p className={styles.title}>페이지를 찾을 수 없습니다.</p>
+      <p className={styles.description}>
+        찾으시는 페이지가 존재하지 않거나 </p>
+        <p className={styles.description}>이동되었을 수 있습니다.</p>
+      <Link href={PATH.HOME} >
+        <button className={styles.button}>홈으로 돌아가기</button>
+      </Link>
+    </div>
+  );
 }
