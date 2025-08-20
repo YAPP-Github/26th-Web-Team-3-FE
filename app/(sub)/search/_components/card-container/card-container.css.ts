cc8628debae69fedfb0ffc5a2c6b64ca850fa620
백승범 <bdh3659@naver.com>
2025-08-20 09:55:06 +0900

    feat: search card container 구현

---
+++ app/(sub)/search/_components/card-container/card-container.css.ts
@@ -0,0 +1,22 @@
+import { style } from "@vanilla-extract/css";
+
+export const cardContainer = style({
+  display: "grid",
+  gap: "1.2rem",
+  rowGap: "3.2rem",
+  width: "100%",
+  padding: "1.6rem",
+  paddingTop: "2.3rem",
+  margin: "0 auto",
+  gridTemplateColumns: "repeat(2, 1fr)",
+
+  // 정확한 반응형 적용하기 위해 미디어 쿼리 직접 사용
+  "@media": {
+    "(min-width: 768px)": {
+      gridTemplateColumns: "repeat(3, 1fr)",
+    },
+    "(min-width: 1024px)": {
+      gridTemplateColumns: "repeat(4, 1fr)",
+    },
+  },
+});
