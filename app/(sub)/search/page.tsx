fa730a6771259440ca5cf487ab29c6e618dc41c1
백승범 <bdh3659@naver.com>
2025-08-20 09:55:31 +0900

    feat: 검색 페이지 구현

--- app/(sub)/search/page.tsx
+++ app/(sub)/search/page.tsx
@@ -1,9 +1,48 @@
+"use client";
+
+import CardContainer from "@/app/(sub)/search/_components/card-container";
 import SearchBarSection from "@/app/(sub)/search/_components/search-bar-section";
+import { capsuleQueryOptions } from "@/shared/api/queries/capsule";
+import LoadingSpinner from "@/shared/ui/loading-spinner";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+import { useIntersectionObserver } from "react-simplikit";
 
 const SearchPage = () => {
+  const searchParams = useSearchParams();
+  const keyword = searchParams.get("keyword") ?? "";
+
+  const {
+    data: searchResults,
+    isLoading,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    ...capsuleQueryOptions.searchCapsuleList(keyword),
+    enabled: keyword.trim().length > 0,
+  });
+
+  const footerRef = useIntersectionObserver<HTMLDivElement>(
+    (entry) => {
+      if (entry.isIntersecting && hasNextPage && !isFetchingNextPage) {
+        fetchNextPage();
+      }
+    },
+    { threshold: 0.8 },
+  );
+
+  const allCapsules =
+    searchResults?.pages.flatMap((page) => page.result.timeCapsules) || [];
+
   return (
     <div>
-      <SearchBarSection />
+      <SearchBarSection keyword={keyword} />
+      <CardContainer capsules={allCapsules} isLoading={isLoading} keyword={keyword} />
+
+      <div ref={footerRef}>
+        {isFetchingNextPage && <LoadingSpinner loading={true} size={20} />}
+      </div>
     </div>
   );
 };
