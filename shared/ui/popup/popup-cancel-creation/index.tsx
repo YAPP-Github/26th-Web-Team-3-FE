1a4c4617a2ffe169eadd90339d315e62fa2dfab1
hanseo <yds06206@gmail.com>
2025-08-21 18:32:16 +0900

    feat: 로딩화면 2초동안 보여지도록 수정

--- shared/ui/popup/popup-cancel-creation/index.tsx
+++ shared/ui/popup/popup-cancel-creation/index.tsx
@@ -12,7 +12,7 @@ const PopupCancelCreation = ({ isOpen, close }: PopupCancelCreationProps) => {
   return (
     <Popup open={isOpen} close={close}>
       <Popup.Title className={styles.title}>만들기를 그만할까요?</Popup.Title>
-      <Popup.Content>
+      <Popup.Content className={styles.content}>
         <p>나가면 처음부터 다시 만들어야 해요 😢</p>
       </Popup.Content>
       <Popup.Actions>
--- shared/ui/popup/popup-cancel-creation/popup-cancel-creation.css.ts
+++ shared/ui/popup/popup-cancel-creation/popup-cancel-creation.css.ts
@@ -4,8 +4,13 @@ import { style } from "@vanilla-extract/css";
 export const title = style({
   ...themeVars.text.F20,
   marginTop: "1rem",
+  marginBottom: "0.6rem",
 });
 
 export const continueButton = style({
   color: themeVars.color.purple[10],
+});
+
+export const content = style({
+  ...themeVars.text.F16,
 });
