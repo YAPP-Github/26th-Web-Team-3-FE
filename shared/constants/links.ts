b95fb1dc9650d714be8fc7c48def49d9da03c3e2
hanseo <yds06206@gmail.com>
2025-08-22 09:23:10 +0900

    feat: 이용약관 링크 연결

---
+++ shared/constants/links.ts
@@ -0,0 +1,10 @@
+export const EXTERNAL_LINKS = [
+  {
+    label: "이용약관",
+    url: "https://www.notion.so/24ecacaffc888073aaebcee3d6aa099c?source=copy_link",
+  },
+  {
+    label: "개인정보 처리방침",
+    url: "https://www.notion.so/24ecacaffc8880b08658fecc47e8aa5c?source=copy_link",
+  },
+] as const;
