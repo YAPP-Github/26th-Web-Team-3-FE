73394f6f25d7d1175361dd21b8500de6a34dfb89
백승범 <bdh3659@naver.com>
2025-08-23 01:06:51 +0900

    Squashed commit of the following:
    commit c07799ad047a6884b563692bafd990439be30e93
    Merge: 0b2fa2b 6d6f73e
    Author: 백승범 <bdh3659@naver.com>
    Date:   Sat Aug 23 00:58:43 2025 +0900
    
        Merge branch 'develop' of https://github.com/YAPP-Github/26th-Web-Team-3-FE into qa/additional/#199
    
    commit 0b2fa2bd9356e7efc5b6778708a11c7500f15870
    Author: 백승범 <bdh3659@naver.com>
    Date:   Sat Aug 23 00:43:48 2025 +0900
    
        style: grid-layout 모바일 view 수정
    
    commit 278afe553b9472fc2b64bde847d30d40c367f436
    Author: 백승범 <bdh3659@naver.com>
    Date:   Sat Aug 23 00:24:26 2025 +0900
    
        fix: 편지 담기 reset 추가
    
    commit 6d6f73e4c18e7f52e9426833ebea07e2dfc0c372
    Author: beom <74394824+seung365@users.noreply.github.com>
    Date:   Sat Aug 23 00:05:59 2025 +0900
    
        fix: 모달 닫힘 경고 팝업 뜨도록 수정 및 esc props 전달 (#198)

--- app/(sub)/capsule-detail/[invite-code]/[id]/letters/_components/grid-layout/grid-layout.css.ts
+++ app/(sub)/capsule-detail/[invite-code]/[id]/letters/_components/grid-layout/grid-layout.css.ts
@@ -3,17 +3,21 @@ import { style } from "@vanilla-extract/css";
 
 export const container = style({
   width: "100vw",
-  padding: "2rem",
   maxWidth: "120rem",
+  padding: "0.8rem",
+  ...screen.md({
+    padding: "1.6rem",
+  }),
 });
 
 export const grid = style({
   display: "grid",
   gridTemplateColumns: "repeat(auto-fill, minmax(16.4rem, 1fr))",
-  gap: "2.8rem 1.2rem",
   margin: "0 auto",
   maxWidth: "120rem",
+  gap: "2.4rem 0.8rem",
   ...screen.md({
     gridTemplateColumns: "repeat(auto-fill, minmax(24rem, 1fr))",
+    gap: "2.8rem 1.2rem",
   }),
 });
